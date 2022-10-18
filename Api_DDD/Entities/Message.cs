a4c17e193c6b7da366cf284dc87d205da6c54c74
Kauê Guerra <kaueguerra@gmail.com>
2022-10-18 09:59:37 -0300

    Adicionar arquivos de projeto.

---
+++ Api_DDD/Entities/Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Table("TB_MESSAGE")]
+    public class Message : Notifies
+    {
+        [Column("MSN_ID")]
+        public int Id { get; set; }
+
+        [Column("MSN_TITLE")]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Column("MSN_ACTIVE")]
+        public bool Active { get; set; }
+
+        [Column("MSN_CREATED_AT")]
+        public DateTime Created_at { get; set; }
+
+        [Column("MSN_UPDATED_AT")]
+        public DateTime Updated_at { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
---
+++ Api_DDD/Entities/Notifies.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notifications = new List<Notifies>();
+        }
+
+        
+        [NotMapped]
+        public string PropertyName { get; set; }
+        public string Message { get; set; }
+        public List<Notifies> Notifications { get; set; }
+
+        public bool ValidatePropertyString(string valor, string propertyName)
+        {
+            if(string.IsNullOrWhiteSpace(propertyName) || string.IsNullOrWhiteSpace(valor))
+            {
+                Notifications.Add(new Notifies
+                {
+                    Message = "Campo Obrigatório",
+                    PropertyName = propertyName,
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidatePropertyInt(int valor, string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName) || valor < 1)
+            {
+                Notifications.Add(new Notifies
+                {
+                    Message = "Campo Obrigatório",
+                    PropertyName = propertyName,
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
