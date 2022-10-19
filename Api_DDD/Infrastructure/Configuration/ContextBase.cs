613d94421ef59ea50dddd1325f9308e273b0d92c
Kauê Guerra <kaueguerra@gmail.com>
2022-10-18 22:25:24 -0300

    Created ContextBase and structure de infra

---
+++ Api_DDD/Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) :base(options)
+        {
+        }
+
+        public DbSet<Message> Message { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnection());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+
+        public string GetStringConnection()
+        {
+            return "Data Source=GUERRA-PC;Initial Catalog=API_DDD;User ID=sa;Password=Ka030393";
+        }
+    }
+}
